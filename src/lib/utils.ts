a7b994c94c3033ef8307b1c4a5983fa9b858479a
who-ash <ayadav9738@gmail.com>
2026-01-03 12:38:36 +0530

    func: added tiptap emptycontent check

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export function isContentEmpty(content: any) {
+    if (!content) return true;
+    if (typeof content !== 'string') return false;
+    const stripped = content.replace(/<[^>]*>/g, '').trim();
+    return stripped === '';
+}
